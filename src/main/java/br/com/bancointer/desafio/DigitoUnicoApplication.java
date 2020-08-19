e7b94aaf5c9f461021733f0ff327b11b1edb2430
Felipe <feliperdaguiar@gmail.com>
2020-08-19 13:58:46 -0300

    Commit inicial

---
+++ src/main/java/br/com/bancointer/desafio/DigitoUnicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancointer.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitoUnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitoUnicoApplication.class, args);
+	}
+
+}
